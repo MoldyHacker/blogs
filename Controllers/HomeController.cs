eefd482d17b4f55c60e8e7484df2666fbc8b6925
moldyhacker <56985918+MoldyHacker@users.noreply.github.com>
2023-02-22 18:47:50 -0600

    sort blogs by name

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 public class HomeController : Controller
 {
-    public IActionResult Index() => View();
+    // this controller depends on the BloggingRepository
+    private DataContext _dataContext;
+    public HomeController(DataContext db) => _dataContext = db;
+
+    public IActionResult Index() => View(_dataContext.Blogs.OrderBy(b => b.Name));
 }
